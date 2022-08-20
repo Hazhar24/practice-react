c85fcbc47dc7f2c58261d86b0d54acfc5f29c0bc
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2022-08-20 17:41:20 +0430

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Layout/Hero";
 import Login from "./components/Layout/login";
+import Navbar from "./components/Layout/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
       <Login />
     </div>
